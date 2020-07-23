0f3b3e3f40d40228742a3bbd264ead7af7e266cb
w3schoolsrus <55655132+w3schoolsrus@users.noreply.github.com>
2020-07-23 13:52:23 +0300

    Update custom-header.php
    Added Russian translation

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Sample implementation of the Custom Header feature
+ * Пример реализации функции пользовательского заголовка
  *
- * You can add an optional custom header image to header.php like so ...
+ * Вы можете добавить необязательный пользовательский заголовок изображения в header.php примерно так ...
  *
 	<?php the_header_image_tag(); ?>
  *
@@ -12,7 +12,7 @@
  */
 
 /**
- * Set up the WordPress core custom header feature.
+ * Настройте основной пользовательский заголовок WordPress.
  *
  * @uses _s_header_style()
  */
@@ -36,7 +36,7 @@ add_action( 'after_setup_theme', '_s_custom_header_setup' );
 
 if ( ! function_exists( '_s_header_style' ) ) :
 	/**
-	 * Styles the header image and text displayed on the blog.
+	 * Стили изображения заголовка и текста, отображаемого в блоге.
 	 *
 	 * @see _s_custom_header_setup().
 	 */
@@ -44,18 +44,18 @@ if ( ! function_exists( '_s_header_style' ) ) :
 		$header_text_color = get_header_textcolor();
 
 		/*
-		 * If no custom options for text are set, let's bail.
-		 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+		 * Если не настроены пользовательские параметры для текста, давайте внесем залог.
+		 * get_header_textcolor() options: Любое шестнадцатеричное значение, пустое, чтобы скрыть текст. По умолчанию: add_theme_support( 'custom-header' ).
 		 */
 		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
 			return;
 		}
 
-		// If we get this far, we have custom styles. Let's do this.
+		// Если мы зайдем так далеко, у нас будут собственные стили. Давай сделаем это.
 		?>
 		<style type="text/css">
 		<?php
-		// Has the text been hidden?
+		// Был ли текст скрыт?
 		if ( ! display_header_text() ) :
 			?>
 			.site-title,
@@ -64,7 +64,7 @@ if ( ! function_exists( '_s_header_style' ) ) :
 				clip: rect(1px, 1px, 1px, 1px);
 				}
 			<?php
-			// If the user has set a custom color for the text use that.
+			// Если пользователь установил пользовательский цвет для текста, используйте его.
 		else :
 			?>
 			.site-title a,
